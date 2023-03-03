56f95d334f244cfc6eabe5682e2b2aaf6217c291
Felipe Manga <fmanga@macmini.Home>
2023-03-03 17:49:36 +0000

    Clean data name

--- dataProg.js
+++ dataProg.js
@@ -1,5 +1,5 @@
 export function dataProg(ast, program, filename, jsc) {
-    const name = filename.split('/').pop().split('.')[0];
+    const name = filename.split('/').pop().split('.')[0].replace(/^[^a-zA-Z_]+|[^a-zA-Z0-9_]+/gi, '');
     // console.log('Adding resource ', name, ast);
     program.resourceData[name] = ast;
     return program;
